6a04390cf492f6e2373a0e68a88783826eb819d5
Imran Hussain <imranhussain399@gmail.com>
2023-06-22 22:28:56 +0500

    Add button to add new book to the library. Also add cancel button to cancel addition of new book

--- libraryApp.js
+++ libraryApp.js
@@ -1,24 +1,47 @@
-// Array for storing all the books objects...
-const booksLibrary = [];
+const addBookButton = document.querySelector(".add-book-button");
+const formContainer = document.querySelector(".form-container");
+const submitButton = document.querySelector("#submit-button");
+const cancelButton = document.querySelector("#cancel-button");
+const form = document.querySelector("form");
+const booksContainer = document.querySelector(".books-container");
 
-const Book = function (title, author, totalPages, isRead) {
+const myBooks = [];
+const Book = function (title, author, pages, isRead) {
      this.title = title;
      this.author = author;
-     this.totalPages = totalPages;
-     this.isRead =isRead;
-}
+     this.pages = pages;
+     this.isRead = isRead;
+};
 
+Book.prototype.addBookToPage = function () {
+     let newBook = document.createElement("p");
+     this.isRead === "yes" ? (newBook.textContent = `${this.title} by ${this.author}, total pages ${this.pages} has been read`) : (newBook.textContent = `${this.title} by ${this.author}, total pages ${this.pages} is not read yet`);
+     booksContainer.appendChild(newBook);
+};
 
-const AddBookToLibrary = function () {
+Book.prototype.addBookToLibrary = function () {
+     this.title = form.elements.title.value;
+     this.author = form.elements.author.value;
+     this.pages = form.elements.pages.value;
+     this.isRead = form.elements["book-read"].value;
+     console.log(this);
+     myBooks.push(this);
+     this.addBookToPage();
+};
+
+addBookButton.addEventListener("click", () => {
+     formContainer.setAttribute("id", "form-container");
+});
+
+submitButton.addEventListener("click", (event) => {
+     event.preventDefault();
      let book = new Book();
-     let inputMessages = ['Book Title?', 'Author Name?', 'Total Pages?', "Has been read? true/false"]; 
-     let prop = Object.keys(book);
-     let index = 0;
-     for (let key of prop) {
-          // book[key] = prompt(inputMessages[index], "")
-          index++;
-     }
-     booksLibrary.push(book);
-}
-AddBookToLibrary();
-console.log(booksLibrary);
+     book.addBookToLibrary();
+     form.reset();
+     formContainer.removeAttribute("id", "form-container");
+});
+
+cancelButton.addEventListener("click", () => {
+     formContainer.removeAttribute("id", "form-container");
+     console.log("cancel button clicked");
+})
